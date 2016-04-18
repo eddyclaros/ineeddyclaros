f0a97125db875a00ea626ffad71526a25eb4e2ff
eclaros <eddyclarosc@gmail.com>
2016-04-18 14:35:58 -0400

    wer

--- app/src/main/java/bo/gob/ine/eclaros/eddyclaroscarritodecompras/MenuPrincipal.java
+++ app/src/main/java/bo/gob/ine/eclaros/eddyclaroscarritodecompras/MenuPrincipal.java
@@ -1,15 +1,26 @@
 package bo.gob.ine.eclaros.eddyclaroscarritodecompras; /**
  * Created by Pc 12 on 17/04/2016.
  */
+import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import bo.gob.ine.eclaros.eddyclaroscarritodecompras.R;
 
 public class MenuPrincipal extends AppCompatActivity {
+    private ImageButton btnAddUsuario;
+    private ImageButton btnAddProducto;
+    private ImageButton btnAddVenta;
+    private ImageButton btnSalir;
+
+    private Context context;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,14 @@ public class MenuPrincipal extends AppCompatActivity {
         setContentView(R.layout.activity_menuprincipal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        context=this;
+
+        btnAddProducto=(ImageButton)findViewById(R.id.btnAddProducto);
+        btnAddUsuario=(ImageButton)findViewById(R.id.btnAddUsuario);
+        btnAddVenta=(ImageButton)findViewById(R.id.btnAddVenta);
+        btnSalir=(ImageButton)findViewById(R.id.btnSalir);
+
 
 
         Intent b=getIntent();
